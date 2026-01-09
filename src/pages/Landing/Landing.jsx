efa42da1ce2b4d9299ac788c5bfd81f77ddc6d0f
Kuol Magok <heiskuol001@gmail.com>
2026-01-09 10:08:45 +0300

    add: a landing page with animated text

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Landing.css'
+
+const Landing = () => {
+
+    return (
+        <div className={'landing-section'}>
+            <h1>TaskPilot</h1>
+        </div>
+    )
+}
+export default Landing
