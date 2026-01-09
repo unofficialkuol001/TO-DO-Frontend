299fbfb3d6de7c24a98e6bd6312136d8caab1e14
Kuol Magok <heiskuol001@gmail.com>
2026-01-09 15:38:43 +0300

    add: home route inserted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Landing from './pages/Landing/Landing.jsx'
 import Login from './pages/Login/Login.jsx'
 import {Routes, Route, Link} from 'react-router-dom'
-import {useNavigate} from 'react-router-dom'
+import Sign from "./pages/Sign/Sign.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 const App = () => {
 
@@ -9,6 +10,8 @@ const App = () => {
     <Routes>
         <Route path="/"  element={<Landing />} />
         <Route path="/login"  element={<Login />} />
+        <Route path={'/sign'} element={<Sign />} />
+        <Route path={'/todo'} element={<Home />} />
     </Routes>
   )
 }
