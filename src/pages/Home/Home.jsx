378fc2c0e3fa2b373d38130f4c2106d055290e13
Kuol Magok <heiskuol001@gmail.com>
2026-01-12 16:48:55 +0300

    add: add functionality to the Add button through the addTask method

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Home.css'
 
 const Home = () => {
+    const [tasks, setTasks] = useState('');
+    const addTask = async ()=>{
+        const res = await fetch("http://localhost:3000/todo/app/addTask",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({tasks: tasks})
+        })
+        if(!res.ok){
+            console.log("error");
+        }
+        const data = res.json();
+        console.log(data);
+    }
     return (
         <div className={'Home'}>
             <div className={'home-header'}>
                 <h1>Welcome</h1>
                 <span>
-                    <input type={'text'} placeholder={'Insert task...'} required={true} />
-                    <button type={'submit'}>Add</button>
+                    <input onChange={(e)=>setTasks(e.target.value)} value={tasks} type={'text'} placeholder={'Insert task...'} required={true} />
+                    <button onClick={addTask} type={'submit'}>Add</button>
                 </span>
             </div>
             <div className={'home-content'}>
